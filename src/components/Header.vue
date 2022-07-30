1b6a0a52665d4e12afe0e3c79631b6e49b865e22
Brown <Brown@hyper-cloud.kr>
2022-07-30 15:36:19 +0900

    fix header input style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,8 +3,8 @@
     <div class="w-screen flex justify-center">
       <div class="max-w-5xl w-full h-24 flex justify-end items-end">
         <div class="bg-white flex px-2 mb-3 rounded-[2px] max-w-[260px] w-full justify-between items-center">
-          <input class="placeholder:text-gray-300 h-8 ml-2 outline-none" placeholder="소환사명,챔피언..." />
-          <IconGG class="w-10 cursor-pointer" />
+          <input class="placeholder:text-gray-300 h-8 ml-2 outline-none text-xs" placeholder="소환사명,챔피언..." />
+          <IconGG class="w-10 cursor-pointer h-3" />
         </div>
       </div>
     </div>
