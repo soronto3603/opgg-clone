d41b61427ec5843f00ce48c6390bf07c0e8dbed7
Brown <Brown@hyper-cloud.kr>
2022-07-30 14:20:47 +0900

    test api request

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
+<script>
+import axios from 'axios'
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+function fetch(summonerName) {
+  return
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+export default {
+  data() {
+    return {
+      user: null,
+    }
+  },
+  mounted() {
+    this.fetch('Hide on bush')
+  },
+  methods: {
+    fetch(summonerName) {
+      return axios
+        .get(`https://codingtest.op.gg/api/summoner/${summonerName}`, {
+          params: {
+            hl: 'ko',
+          },
+        })
+        .then(({ data }) => {
+          console.log(data)
+          this.user = data
+        })
+    },
+  },
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
