f3c1a9c935979a4355ec68a7f07163e7e251c525
Brown <Brown@hyper-cloud.kr>
2022-07-30 14:08:12 +0900

    eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-await-in-loop': 'warn',
+    'arrow-body-style': ['warn', 'as-needed'],
+  },
+}
